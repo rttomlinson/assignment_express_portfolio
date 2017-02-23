e948f5512c85b6d0e3bfce987b2f462b55a578ef
Renzo Tomlinson <rttomlinson@gmail.com>
2017-02-23 01:34:44 +0000

    Add projects.hbs, modules/projects.js, index.js to render project.hbs page

---
+++ modules/projects.js
@@ -0,0 +1,19 @@
+"use strict"; //Warming this does not reinforce unquie idenifiers. Highly recommended to create with unique identifiers and store in keys
+
+class Project {
+    constructor(name, url, description, imageSource, technologies) {
+        this.name = name;
+        this.url = url;
+        this.description = description;
+        this.imageSource = imageSource;
+        this.technologies = technologies;
+    }
+}
+
+const projects = [
+    new Project("Weather App", "https://rttomlinson.github.io/weather_app/", "A weather app using Dark Sky for weather information", "http://placehold.it/350x300", ["jQuery"]),
+    new Project("Royalty Free Music Player", "https://rttomlinson.github.io/assignment_royalty_free_music_player/", "A project focused on eventhandlers", "http://placehold.it/350x300", ["jQuery", "Web Audio API"])
+    ];
+    
+    
+module.exports = projects;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
+"use strict";
+
 var express = require('express');
 var router = express.Router();
 
+let projects = require("../modules/projects");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('projects', { projects: projects });
 });
 
 module.exports = router;
