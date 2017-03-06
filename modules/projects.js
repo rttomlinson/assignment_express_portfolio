0bf3f1970a4f9944cb6f3dbc226bdcf5954687de
Renzo Tomlinson <rttomlinson@gmail.com>
2017-03-06 08:57:52 +0000

    Add Who Are My Reps to projects

--- modules/projects.js
+++ modules/projects.js
@@ -1,18 +1,21 @@
 "use strict"; //Warming this does not reinforce unquie idenifiers. Highly recommended to create with unique identifiers and store in keys
 
 class Project {
-    constructor(name, url, description, imageSource, technologies) {
+    constructor(name, url, description, imageSource, technologies, github, lastUpdate) {
         this.name = name;
         this.url = url;
         this.description = description;
         this.imageSource = imageSource;
         this.technologies = technologies;
+        this.github = github;
+        this.lastUpdate = lastUpdate;
     }
 }
 
 const projects = [
-    new Project("Weather App", "https://rttomlinson.github.io/weather_app/", "A weather app using Dark Sky for weather information", "http://placehold.it/350x300", ["jQuery"]),
-    new Project("Royalty Free Music Player", "https://rttomlinson.github.io/assignment_royalty_free_music_player/", "A project focused on eventhandlers", "http://placehold.it/350x300", ["jQuery", "Web Audio API"])
+    new Project("Who Are My Reps", "https://who-are-my-reps.herokuapp.com/", "Find your representatives by zipcode and their recent voting history", "http://placehold.it/350x300", ["Express", "Sunlight API"], "https://github.com/rttomlinson/rep_finder", null),
+    new Project("Royalty Free Music Player", "https://free-music-player.herokuapp.com/", "A project focused on eventhandlers", "http://placehold.it/350x300", ["jQuery", "Web Audio API"], "https://github.com/rttomlinson/assignment_royalty_free_music_player", null),
+    new Project("Weather App", "https://rttomlinson.github.io/weather_app/", "A weather app using Dark Sky for weather information", "http://placehold.it/350x300", ["jQuery"], "https://github.com/rttomlinson/weather_app", null)
     ];
     
     
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,46 @@
 
 var express = require('express');
 var router = express.Router();
-
+const http = require('http');
 let {projects} = require("../modules/projects");
+
+http.get('https://rttomlinson-public-projects.herokuapp.com/', (res) => {
+    const statusCode = res.statusCode;
+    const contentType = res.headers['content-type'];
+    
+    let error;
+    
+    if (statusCode !== 200) {
+        error = new Error(`Request failed.\n Status code: ${statusCode}`);
+    } else if (!/^application\/json/.test(contentType)) {
+        error = new Error(`Invalid content type.\n Content type is: ${contentType}. Need JSON. Check request headers`);
+    }
+    if (error) {
+        console.log(error);
+        res.resume();
+        return;
+    }
+    
+    res.setEncoding('utf-8');
+    let rawData = '';
+    res.on('data', (chunk) => rawData += chunk);
+    res.on('end', () => {
+        try {
+            let parsedData = JSON.parse(rawData);
+            console.log(parsedData);
+        }
+        catch(e) {
+            console.log(`An error occured in parsing JSON try/catch: ${e.message}`);
+        }
+    });
+
+}).on('error', (e) => {
+    console.log(`Error message caught by http get request: ${e.message}`);
+});
+
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
