c16f7752f0def583a34bda1e457017fb30a5d0ba
Renzo Tomlinson <rttomlinson@gmail.com>
2017-03-06 09:28:58 +0000

    Add image urls for Who Are My Reps and Royalty Free Music Player

--- modules/projects.js
+++ modules/projects.js
@@ -13,8 +13,8 @@ class Project {
 }
 
 const projects = [
-    new Project("Who Are My Reps", "https://who-are-my-reps.herokuapp.com/", "Find your representatives by zipcode and their recent voting history", "http://placehold.it/350x300", ["Express", "Sunlight API"], "https://github.com/rttomlinson/rep_finder"),
-    new Project("Royalty Free Music Player", "https://free-music-player.herokuapp.com/", "A project focused on eventhandlers", "http://placehold.it/350x300", ["jQuery", "Web Audio API"], "https://github.com/rttomlinson/assignment_royalty_free_music_player"),
+    new Project("Who Are My Reps", "https://who-are-my-reps.herokuapp.com/", "Find your representatives by zipcode and their recent voting history", "http://res.cloudinary.com/dsuajh1lm/image/upload/c_scale,w_450/v1488792316/whoaremyreps_jumpwi.png", ["Express", "Sunlight API"], "https://github.com/rttomlinson/rep_finder"),
+    new Project("Royalty Free Music Player", "https://free-music-player.herokuapp.com/", "A project focused on eventhandlers", "http://res.cloudinary.com/dsuajh1lm/image/upload/c_scale,w_450/v1488792315/royaltyfreemusicplayer_h40eja.png", ["jQuery", "Web Audio API"], "https://github.com/rttomlinson/assignment_royalty_free_music_player"),
     new Project("Weather App", "https://rttomlinson.github.io/weather_app/", "A weather app using Dark Sky for weather information", "http://placehold.it/350x300", ["jQuery"], "https://github.com/rttomlinson/weather_app")
     ];
     
